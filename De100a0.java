0ba5fe06cc82f85651e788b3b4276c68b60615d4
Vinicius Tork <torkdp43@gmail.com>
2023-09-03 23:53:56 -0300

    Primeiro Commit

---
+++ De100a0.java
@@ -0,0 +1,13 @@
+
+public class De100a0 {
+    public static void main(String[] args) {
+        int x;
+        
+        x=0;
+        
+        while (x!=101) {            
+            System.out.println("Numero:"+x);
+            x=x+1;
+        }
+    }
+}
